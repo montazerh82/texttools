050fef704991cc2a73993b735804d7361878aa18
Tohidi <the.mohammad.tohidi@gmail.com>
2025-04-27 12:38:47 +0330

    feat: turned into a library-> v 0.0.1

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="texttools",
+    version="0.0.1",
+    packages=find_packages(),
+    author="Tohidi",
+    description="set of my text tools"   
+    
+)
