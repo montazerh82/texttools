6011b48bade03637dfffb1574c08cb9f5acb6d2b
Tohidi <the.mohammad.tohidi@gmail.com>
2025-04-28 17:06:47 +0330

    fix: imports

--- texttools/__init__.py
+++ texttools/__init__.py
@@ -1,6 +1,6 @@
 from texttools.question_detector.llm_detector import LLMQuestionDetector
 from texttools.handlers import NoOpResultHandler, PrintResultHandler, ResultHandler, SaveToFileResultHandler
-from texttools import EmbeddingCategorizer
+from texttools.categorizer.encoder_model.encoder_vectorizer import EmbeddingCategorizer
 from texttools.categorizer.llm.openai_categorizer import LLMCategorizer
 from texttools.categorizer.offline_llm.offline_categorizer import OfflineCategorizer
 from texttools.question_detector.offline_llm_detector import OfflineQuestionDetector
