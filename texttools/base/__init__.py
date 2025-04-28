9d2d94d0880a20a818a691c82e78b2e7fff98aaa
Tohidi <the.mohammad.tohidi@gmail.com>
2025-04-28 16:59:00 +0330

    fix: init

--- texttools/base/__init__.py
+++ texttools/base/__init__.py
@@ -1,3 +1,3 @@
-from base_categorizer import *
-from offline_batch_processor import *
-from base_question_detector import *
+from texttools.base.base_categorizer import *
+from texttools.base.offline_batch_processor import *
+from texttools.base.base_question_detector import *
