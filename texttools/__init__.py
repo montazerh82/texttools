3c213ea9692e88e93934cbcb56c7dfbb3b95dfa0
Tohidi <the.mohammad.tohidi@gmail.com>
2025-04-29 15:13:58 +0330

    fix: import

--- texttools/__init__.py
+++ texttools/__init__.py
@@ -2,7 +2,7 @@ from texttools.question_detector.llm_detector import LLMQuestionDetector
 from texttools.handlers import NoOpResultHandler, PrintResultHandler, ResultHandler, SaveToFileResultHandler
 from texttools.categorizer.encoder_model.encoder_vectorizer import EmbeddingCategorizer
 from texttools.categorizer.llm.openai_categorizer import LLMCategorizer
-from texttools.batch_manager.batch_manager import SimpleBatchManager
+from texttools.batch_manager import SimpleBatchManager
 
 __all__ = [
     "LLMQuestionDetector",
